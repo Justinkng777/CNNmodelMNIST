f4194c42df4be27ddfac8c9e45c935910c290b30
Justin Ng <justinkng777@gmail.com>
2024-06-26 17:33:03 -0400

    initial imports

--- src/MnistTraining.py
+++ src/MnistTraining.py
@@ -2,4 +2,12 @@
 By justin Ng
 Following tutorial by Machine learning mastery's article 
 "How to Develop a CNN for MNIST Handwritten Digit Classification" by Jason Brownlee
+
+Split the MNIST data set into training and testing
+we will have to 
+Develope baseline model
+improve it 
+and finilize and make predictions
 """
+from keras.api.datasets import mnist
+from matplotlib import pyplot as plt
